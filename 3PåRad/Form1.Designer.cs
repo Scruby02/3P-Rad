261934ece65807be399fbc491031340d40669bc5
Ole Einar Fauske Hoch Nielsen <OleEinarFHN@gmail.com>
2020-09-19 00:40:26 +0200

    Added Buttons

--- 3PåRad/Form1.Designer.cs
+++ 3PåRad/Form1.Designer.cs
@@ -28,25 +28,121 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.B1_1 = new System.Windows.Forms.Button();
+            this.B11 = new System.Windows.Forms.Button();
+            this.B12 = new System.Windows.Forms.Button();
+            this.B13 = new System.Windows.Forms.Button();
+            this.B21 = new System.Windows.Forms.Button();
+            this.B22 = new System.Windows.Forms.Button();
+            this.B23 = new System.Windows.Forms.Button();
+            this.B31 = new System.Windows.Forms.Button();
+            this.B32 = new System.Windows.Forms.Button();
+            this.B33 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // B1_1
+            // B11
             // 
-            this.B1_1.Location = new System.Drawing.Point(12, 306);
-            this.B1_1.Name = "B1_1";
-            this.B1_1.Size = new System.Drawing.Size(120, 132);
-            this.B1_1.TabIndex = 0;
-            this.B1_1.Text = "1:1";
-            this.B1_1.UseVisualStyleBackColor = true;
-            this.B1_1.Click += new System.EventHandler(this.B1_1_Click);
+            this.B11.Font = new System.Drawing.Font("Dubai", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.B11.Location = new System.Drawing.Point(12, 288);
+            this.B11.Name = "B11";
+            this.B11.Size = new System.Drawing.Size(120, 132);
+            this.B11.TabIndex = 0;
+            this.B11.UseVisualStyleBackColor = true;
+            this.B11.Click += new System.EventHandler(this.B11_Click);
+            // 
+            // B12
+            // 
+            this.B12.Font = new System.Drawing.Font("Dubai", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.B12.Location = new System.Drawing.Point(138, 288);
+            this.B12.Name = "B12";
+            this.B12.Size = new System.Drawing.Size(120, 132);
+            this.B12.TabIndex = 1;
+            this.B12.UseVisualStyleBackColor = true;
+            this.B12.Click += new System.EventHandler(this.B12_Click);
+            // 
+            // B13
+            // 
+            this.B13.Font = new System.Drawing.Font("Dubai", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.B13.Location = new System.Drawing.Point(263, 288);
+            this.B13.Name = "B13";
+            this.B13.Size = new System.Drawing.Size(120, 132);
+            this.B13.TabIndex = 2;
+            this.B13.UseVisualStyleBackColor = true;
+            this.B13.Click += new System.EventHandler(this.B13_Click);
+            // 
+            // B21
+            // 
+            this.B21.Font = new System.Drawing.Font("Dubai", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.B21.Location = new System.Drawing.Point(12, 150);
+            this.B21.Name = "B21";
+            this.B21.Size = new System.Drawing.Size(120, 132);
+            this.B21.TabIndex = 3;
+            this.B21.UseVisualStyleBackColor = true;
+            this.B21.Click += new System.EventHandler(this.B21_Click);
+            // 
+            // B22
+            // 
+            this.B22.Font = new System.Drawing.Font("Dubai", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.B22.Location = new System.Drawing.Point(138, 150);
+            this.B22.Name = "B22";
+            this.B22.Size = new System.Drawing.Size(120, 132);
+            this.B22.TabIndex = 4;
+            this.B22.UseVisualStyleBackColor = true;
+            this.B22.Click += new System.EventHandler(this.B22_Click);
+            // 
+            // B23
+            // 
+            this.B23.Font = new System.Drawing.Font("Dubai", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.B23.Location = new System.Drawing.Point(264, 150);
+            this.B23.Name = "B23";
+            this.B23.Size = new System.Drawing.Size(120, 132);
+            this.B23.TabIndex = 5;
+            this.B23.UseVisualStyleBackColor = true;
+            this.B23.Click += new System.EventHandler(this.B23_Click);
+            // 
+            // B31
+            // 
+            this.B31.Font = new System.Drawing.Font("Dubai", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.B31.Location = new System.Drawing.Point(12, 12);
+            this.B31.Name = "B31";
+            this.B31.Size = new System.Drawing.Size(120, 132);
+            this.B31.TabIndex = 6;
+            this.B31.UseVisualStyleBackColor = true;
+            this.B31.Click += new System.EventHandler(this.B31_Click);
+            // 
+            // B32
+            // 
+            this.B32.Font = new System.Drawing.Font("Dubai", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.B32.Location = new System.Drawing.Point(138, 12);
+            this.B32.Name = "B32";
+            this.B32.Size = new System.Drawing.Size(120, 132);
+            this.B32.TabIndex = 7;
+            this.B32.UseVisualStyleBackColor = true;
+            this.B32.Click += new System.EventHandler(this.B32_Click);
+            // 
+            // B33
+            // 
+            this.B33.Font = new System.Drawing.Font("Dubai", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.B33.Location = new System.Drawing.Point(263, 12);
+            this.B33.Name = "B33";
+            this.B33.Size = new System.Drawing.Size(120, 132);
+            this.B33.TabIndex = 8;
+            this.B33.UseVisualStyleBackColor = true;
+            this.B33.Click += new System.EventHandler(this.B33_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.B1_1);
+            this.ClientSize = new System.Drawing.Size(393, 425);
+            this.Controls.Add(this.B33);
+            this.Controls.Add(this.B32);
+            this.Controls.Add(this.B31);
+            this.Controls.Add(this.B23);
+            this.Controls.Add(this.B22);
+            this.Controls.Add(this.B21);
+            this.Controls.Add(this.B13);
+            this.Controls.Add(this.B12);
+            this.Controls.Add(this.B11);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -55,7 +151,15 @@
 
         #endregion
 
-        private System.Windows.Forms.Button B1_1;
+        private System.Windows.Forms.Button B11;
+        private System.Windows.Forms.Button B12;
+        private System.Windows.Forms.Button B13;
+        private System.Windows.Forms.Button B21;
+        private System.Windows.Forms.Button B22;
+        private System.Windows.Forms.Button B23;
+        private System.Windows.Forms.Button B31;
+        private System.Windows.Forms.Button B32;
+        private System.Windows.Forms.Button B33;
     }
 }
 
--- 3PåRad/Form1.cs
+++ 3PåRad/Form1.cs
@@ -12,14 +12,81 @@ namespace _3PåRad
 {
     public partial class Form1 : Form
     {
+        Button PressedButton;
+        Boolean XTurn = true;
+
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void B1_1_Click(object sender, EventArgs e)
+        private void ButtonText()
         {
+            if (XTurn == true)
+            {
+                XTurn = false;
+                PressedButton.Text = "X";
+            }
+            else if (XTurn == false)
+            {
+                XTurn = true;
+                PressedButton.Text = "O";
+            }
+            PressedButton.Enabled = false;
+        }
 
+        private void B11_Click(object sender, EventArgs e)
+        {
+            PressedButton = B11;
+            ButtonText();
+        }
+
+        private void B12_Click(object sender, EventArgs e)
+        {
+            PressedButton = B12;
+            ButtonText();
+        }
+
+        private void B13_Click(object sender, EventArgs e)
+        {
+            PressedButton = B13;
+            ButtonText();
+        }
+
+        private void B21_Click(object sender, EventArgs e)
+        {
+            PressedButton = B21;
+            ButtonText();
+        }
+
+        private void B22_Click(object sender, EventArgs e)
+        {
+            PressedButton = B22;
+            ButtonText();
+        }
+
+        private void B23_Click(object sender, EventArgs e)
+        {
+            PressedButton = B23;
+            ButtonText();
+        }
+
+        private void B31_Click(object sender, EventArgs e)
+        {
+            PressedButton = B31;
+            ButtonText();
+        }
+
+        private void B32_Click(object sender, EventArgs e)
+        {
+            PressedButton = B32;
+            ButtonText();
+        }
+
+        private void B33_Click(object sender, EventArgs e)
+        {
+            PressedButton = B33;
+            ButtonText();
         }
     }
 }
